c3ac6e101538cfa1fedacc9d597fd89745c04261
Rog3rSm1th <r0g3r5@protonmail.com>
2024-04-10 23:48:21 +0200

    Update `Function` struct and add `push_statement` method

--- sierra-analyzer/src/decompiler/function.rs
+++ sierra-analyzer/src/decompiler/function.rs
@@ -1,13 +1,34 @@
 use cairo_lang_sierra::program::GenFunction;
+use cairo_lang_sierra::program::Statement;
 use cairo_lang_sierra::program::StatementIdx;
 
-#[allow(dead_code)]
+/// A struct representing a control flow graph (CFG) for a function
 pub struct ControlFlowGraph {}
+
 impl ControlFlowGraph {}
 
-#[allow(dead_code)]
+/// A struct representing a function in a Sierra program
 pub struct Function<'a> {
+    /// The function's `GenFunction` representation
     function: GenFunction<&'a StatementIdx>,
-    // TODO : Add statements
+    /// A vector of `Statement`s representing the function's body
+    statements: Vec<Statement>,
+    /// A `ControlFlowGraph` representing the function's CFG
     cfg: ControlFlowGraph,
+}
+
+impl<'a> Function<'a> {
+    /// Creates a new `Function` instance
+    pub fn new(function: GenFunction<&'a StatementIdx>, cfg: ControlFlowGraph) -> Self {
+        Self {
+            function,
+            statements: Vec::new(),
+            cfg,
+        }
+    }
+
+    /// Adds a `Statement` to the function's body
+    pub fn push_statement(&mut self, statement: Statement) {
+        self.statements.push(statement);
+    }
 }
