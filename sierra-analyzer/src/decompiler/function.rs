bd557377ab0c27e59ec9034417e0cf07e5ebdeaa
Rog3rSm1th <r0g3r5@protonmail.com>
2024-04-17 15:08:50 +0200

    Enhance statements formatting

--- sierra-analyzer/src/decompiler/function.rs
+++ sierra-analyzer/src/decompiler/function.rs
@@ -6,6 +6,38 @@ use cairo_lang_sierra::program::StatementIdx;
 use crate::decompiler::cfg::ControlFlowGraph;
 use crate::decompiler::cfg::SierraConditionalBranch;
 
+/// Macro to parse the debug name from a libfunc_id,
+/// using the debug_name if present or falling back to the id field
+macro_rules! parse_libfunc_name {
+    ($libfunc_id:expr) => {
+        if let Some(debug_name) = &$libfunc_id.debug_name {
+            debug_name.to_string()
+        } else {
+            $libfunc_id.id.to_string()
+        }
+    };
+}
+
+/// Macro to extract parameters from the args field of a GenInvocation object.
+/// It converts each parameter into a String, using the debug_name if available,
+/// otherwise using the id field.
+macro_rules! extract_parameters {
+    ($args:expr) => {
+        $args
+            .iter()
+            .map(|var_id| {
+                if let Some(debug_name) = &var_id.debug_name {
+                    // If debug_name exists, use it as parameter
+                    debug_name.clone().into()
+                } else {
+                    // If debug_name is None, use id field as parameter
+                    format!("v{}", var_id.id)
+                }
+            })
+            .collect::<Vec<String>>()
+    };
+}
+
 /// A struct representing a statement in a Sierra program with an offset
 #[derive(Debug, Clone)]
 pub struct SierraStatement {
@@ -35,9 +67,52 @@ impl SierraStatement {
     }
 
     /// Formats the statement as a string
-    /// TODO : More readable statements representations
     pub fn formatted_statement(&self) -> String {
-        self.statement.to_string()
+        match &self.statement {
+            // Return statements
+            GenStatement::Return(vars) => {
+                let mut formatted = String::from("return (");
+                for (index, var) in vars.iter().enumerate() {
+                    if index > 0 {
+                        formatted.push_str(", ");
+                    }
+                    formatted.push_str(&format!("v{}", var.id));
+                }
+                formatted.push_str(")");
+                formatted
+            }
+            // Function calls & variables assignations
+            GenStatement::Invocation(invocation) => {
+                // Function name
+                let libfunc_id_str = parse_libfunc_name!(invocation.libfunc_id);
+
+                // Function parameters
+                let parameters = extract_parameters!(invocation.args);
+                let parameters_str = parameters.join(", ");
+
+                // Assigned variables
+                let assigned_variables = extract_parameters!(&invocation
+                    .branches
+                    .first()
+                    .map(|branch| &branch.results)
+                    .unwrap_or(&vec![]));
+                let assigned_variables_str = if !assigned_variables.is_empty() {
+                    assigned_variables.join(", ")
+                } else {
+                    String::new()
+                };
+
+                // Format the string based on the presence of assigned variables
+                if !assigned_variables.is_empty() {
+                    format!(
+                        "{} = {}({})",
+                        assigned_variables_str, libfunc_id_str, parameters_str
+                    )
+                } else {
+                    format!("{}({})", libfunc_id_str, parameters_str)
+                }
+            }
+        }
     }
 
     /// Returns a reference to this statement as a conditional branch if it is one
@@ -48,24 +123,10 @@ impl SierraStatement {
                 let statement = self.statement.clone();
 
                 // Function name
-                let libfunc_id_str = if let Some(debug_name) = &invocation.libfunc_id.debug_name {
-                    debug_name.to_string()
-                } else {
-                    invocation.libfunc_id.id.to_string()
-                };
+                let libfunc_id_str = parse_libfunc_name!(invocation.libfunc_id);
 
                 // Parameters
-                let parameters: Vec<String> = invocation
-                    .args
-                    .iter()
-                    .map(|var_id| {
-                        if let Some(debug_name) = &var_id.debug_name {
-                            debug_name.clone().into()
-                        } else {
-                            format!("v{}", var_id.id)
-                        }
-                    })
-                    .collect();
+                let parameters = extract_parameters!(invocation.args);
 
                 // Fallthrough
                 let fallthrough = invocation
