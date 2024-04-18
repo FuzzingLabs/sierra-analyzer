75db7e5f3510d7471254a50216f6fcf5b747cd66
Rog3rSm1th <r0g3r5@protonmail.com>
2024-04-18 02:12:39 +0200

    Enable decompilation of serialized Sierra programs

--- sierra-analyzer/src/main.rs
+++ sierra-analyzer/src/main.rs
@@ -1,8 +1,12 @@
-use sierra_analyzer_lib::sierra_program;
+use serde_json;
 use std::env;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
+
+use cairo_lang_starknet_classes::contract_class::ContractClass;
+
+use sierra_analyzer_lib::sierra_program;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,14 +16,32 @@ fn main() {
         return;
     }
 
+    // Read input file
     let path = Path::new(&args[1]);
     let mut file = File::open(&path).expect("Failed to open file");
     let mut content = String::new();
     file.read_to_string(&mut content)
         .expect("Failed to read file");
 
-    let program = sierra_program::SierraProgram::new(content);
-    let mut decompiler = program.decompiler();
+    // Decompile a serialized Sierra program
+    // Deserialize the JSON content into a ContractClass
+    if let Ok(prog) = serde_json::from_str::<ContractClass>(&content) {
+        let prog_sierra = prog.extract_sierra_program().unwrap();
 
-    println!("{}", decompiler.decompile());
+        // Convert Sierra program to a string
+        let prog_sierra_string = format!("{}", prog_sierra.to_string());
+        let program = sierra_program::SierraProgram::new(prog_sierra_string);
+
+        // Decompile
+        let mut decompiler = program.decompiler();
+        println!("{}", decompiler.decompile());
+    } 
+    // Decompile a Sierra program
+    else {
+        let program = sierra_program::SierraProgram::new(content);
+
+        // Decompile
+        let mut decompiler = program.decompiler();
+        println!("{}", decompiler.decompile());
+    }
 }
